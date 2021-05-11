babc14c8b58e453bedf094b222e7a87f65d391b4
ledong1 <ledong.shi17@stjohns.edu>
2021-05-11 15:33:14 -0400

    增加英雄语音

--- samples/python/guessMusic.py
+++ samples/python/guessMusic.py
@@ -5,5 +5,11 @@ from wechat import WeChatManager, MessageType
 wechat_manager = WeChatManager(libs_path='../../libs')
 import json
 
+hvSize={'巫毒师 巴蒂斯特': 15, '盲豹 格雷': 6,'鹰眼 凯思卓': 6,'天使 奥达基': 14,'机械战姬 阿尔法': 15,'刀锋魅影 安卡': 15,'护卫 亚丹': 8}
+hNickname={'巫毒师 巴蒂斯特': ['巴蒂斯特','巴蒂'], '盲豹 格雷': 6,'鹰眼 凯思卓': 6,'天使 奥达基': 14,'机械战姬 阿尔法': 15,'刀锋魅影 安卡': 15,'护卫 亚丹': 8}
+
+def guessHero(client_id, chatRoom):
+
+
 def sentVoice(client_id, chatRoom, file, id):
     wechat_manager.send_video(client_id, chatRoom, file)
